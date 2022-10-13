b3aa3950bca2d90dc6125eef141dea5900b4bff5
Wonseok Park <amurorei2000@gmail.com>
2022-10-13 11:38:21 +0900

    Grab Release

--- Assets/Scripts/InputTest.cs
+++ Assets/Scripts/InputTest.cs
@@ -13,60 +13,140 @@ public class InputTest : MonoBehaviour
     public OVRHand rightHand;
     public OVRInput.Controller myController;
 
+    Transform grabedObject_Left;
+    Transform grabedObject_Right;
+
     // Start is called before the first frame update
     void Start()
     {
-         //MTVS_Input.GetOriginTransform();
+        if (VibrationManager.instance == null)
+        {
+            new GameObject("ViberationManager", typeof(VibrationManager));
+        }
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if(MTVS_Input.GetDown(MTVS_Input.Button.Jump, MTVS_Input.Controller.RTouch))
-        {
-
-        }
-
-        // 1. 오른손 콘트롤러의 인덱스 트리거 버튼을 눌렀을 때
-        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
-        {
-            print("버튼 눌렀다!");
-            rightHandText.text = "버튼 눌렀다!";
-        }
-
-        // 2. 오른손 콘트롤러의 인덱스 트리거 버튼을 누르고 있는 내내
-        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
-        {
-            print("버튼 꾹!");
-        }
-
-        // 3. 오른손 콘트롤러의 인덱스 트리거 버튼을 뗐을 때
-        if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, myController))
-        {
-            print("버튼 뗐다!");
-            rightHandText.text = "버튼 뗐다!";
-        }
-        
-
-        Vector2 axis = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, OVRInput.Controller.LTouch);
-        float triggerPow = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.LTouch);
-        
-        leftHandText.text = $"X축 : {axis.x} \r\n Y축: {axis.y} \r\n 트리거: {triggerPow}";
-
-        // 손가락을 집었을 때 집었다고 출력하자!
-        float strength = rightHand.GetFingerPinchStrength(OVRHand.HandFinger.Index);
-        logText.text = strength.ToString();
-        logText.text += "\r\n" + rightHand.GetFingerConfidence(OVRHand.HandFinger.Index).ToString();
-        //if(rightHand.GetFingerIsPinching(OVRHand.HandFinger.Middle))
+        #region 입력 테스트
+        //// 1. 오른손 콘트롤러의 인덱스 트리거 버튼을 눌렀을 때
+        //if (MTVS_Input.GetDown(MTVS_Input.Button.Fire1, MTVS_Input.Controller.RTouch))
         //{
-        //    logText.text = "집었다!";
+        //    print("버튼 눌렀다!");
+        //    rightHandText.text = "버튼 눌렀다!";
         //}
-        //else
+
+        //// 2. 오른손 콘트롤러의 인덱스 트리거 버튼을 누르고 있는 내내
+        //if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
         //{
-        //    logText.text = "뗐다!";
+        //    print("버튼 꾹!");
         //}
+
+        //// 3. 오른손 콘트롤러의 인덱스 트리거 버튼을 뗐을 때
+        //if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, myController))
+        //{
+        //    print("버튼 뗐다!");
+        //    rightHandText.text = "버튼 뗐다!";
+        //}
+
+
+        //Vector2 axis = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, OVRInput.Controller.LTouch);
+        //float triggerPow = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.LTouch);
+
+        //leftHandText.text = $"X축 : {axis.x} \r\n Y축: {axis.y} \r\n 트리거: {triggerPow}";
+
+        //// 손가락을 집었을 때 집었다고 출력하자!
+        //float strength = rightHand.GetFingerPinchStrength(OVRHand.HandFinger.Index);
+        //logText.text = strength.ToString();
+        //logText.text += "\r\n" + rightHand.GetFingerConfidence(OVRHand.HandFinger.Index).ToString();
+        ////if(rightHand.GetFingerIsPinching(OVRHand.HandFinger.Middle))
+        ////{
+        ////    logText.text = "집었다!";
+        ////}
+        ////else
+        ////{
+        ////    logText.text = "뗐다!";
+        ////}
+        #endregion
+
+        //if (MTVS_Input.GetDown(MTVS_Input.Button.Fire1, MTVS_Input.Controller.RTouch))
+        //{
+        //    VibrationManager.instance.StartViberation(0.5f, 0.5f, 0.5f, MTVS_Input.Controller.RTouch);
+        //}
+
+        if(MTVS_Input.GetDown(MTVS_Input.Button.Fire2, MTVS_Input.Controller.LTouch))
+        {
+            Grab(true);
+        }
+        else if(MTVS_Input.GetUp(MTVS_Input.Button.Fire2, MTVS_Input.Controller.LTouch))
+        {
+            Release(true);
+        }
+
 
     }
+
+    // 물건 잡기
+    void Grab(bool isLeft)
+    {
+        Transform grabed = isLeft ? grabedObject_Left : grabedObject_Right;
+        if(grabed)
+        {
+            return;
+        }
+
+        Vector3 handPosition = isLeft ? MTVS_Input.LeftHandPosition : MTVS_Input.RightHandPosition;
+        Collider[] cols = Physics.OverlapSphere(handPosition, 1, 1 << 6);
+
+        if (cols.Length > 0)
+        {
+            print(cols[0].name);
+            Rigidbody rb = cols[0].GetComponent<Rigidbody>();
+            if (rb)
+            {
+                rb.isKinematic = true;
+            }
+            cols[0].transform.parent = isLeft ? MTVS_Input.LeftHandTransform : MTVS_Input.RightHandTransform;
+            cols[0].transform.localPosition = Vector3.zero;
+            
+            if(isLeft)
+            {
+                grabedObject_Left = cols[0].transform;
+            }
+            else
+            {
+                grabedObject_Right = cols[0].transform;
+            }
+        }
+    }
+
+    // 물건 놓기
+    void Release(bool isLeft)
+    {
+        Transform grabed = isLeft ? grabedObject_Left : grabedObject_Right;
+        if (!grabed)
+        {
+            return;
+        }
+
+        grabed.parent = null;
+        Rigidbody rb = grabed.GetComponent<Rigidbody>();
+
+        if(rb)
+        {
+            rb.isKinematic = false;
+        }
+
+        if(isLeft)
+        {
+            grabedObject_Left = null;
+        }
+        else
+        {
+            grabedObject_Right = null;
+        }
+    }
+
 }
--- Assets/Scripts/MTVS_Input.cs
+++ Assets/Scripts/MTVS_Input.cs
@@ -9,29 +9,30 @@ namespace MTVS_InputName
     {
         public enum Button
         {
-            Fire1,
-            Fire2,
-            Fire3,
-            Jump,
-        }
-
-        public enum ButtonTarget
-        {
 #if PC
             Fire1,
             Fire2,
             Fire3,
             Jump,
-//#elif Oculus
-
+#elif Oculus
+            Fire1 = OVRInput.Button.PrimaryIndexTrigger,
+            Fire2 = OVRInput.Button.PrimaryHandTrigger,
+            Fire3 = OVRInput.Button.Two,
+            Jump = OVRInput.Button.One
 #endif
         }
 
 
+
         public enum Controller
         {
+#if PC
             LTouch,
             RTouch,
+#elif Oculus
+            LTouch = OVRInput.Controller.LTouch,
+            RTouch = OVRInput.Controller.RTouch,
+#endif
 
         }
 
@@ -92,14 +93,118 @@ namespace MTVS_InputName
             }
         }
 
+        static Transform leftHandTransform;
+        public static Transform LeftHandTransform
+        {
+            get
+            {
+                leftHandTransform = GetOriginTransform().Find("LeftHandAnchor");
+                return leftHandTransform;
+            }
+        }
+
+        static Transform rightHandTransform;
+        public static Transform RightHandTransform
+        {
+            get
+            {
+                rightHandTransform = GetOriginTransform().Find("RightHandAnchor");
+                return rightHandTransform;
+            }
+        }
+  
+
         // 입력 함수 3종
         public static bool GetDown(Button input, Controller mask)
         {
+#if PC
+            return Input.GetButtonDown(input.ToString());
+#elif Oculus
+            return OVRInput.GetDown((OVRInput.Button)input, (OVRInput.Controller)mask);
+#endif
 
-            return false;
+        }
+
+        public static bool Get(Button input, Controller mask)
+        {
+#if PC
+            return Input.GetButton(input.ToString());
+#elif Oculus
+            return OVRInput.Get((OVRInput.Button)input, (OVRInput.Controller)mask);
+#endif
+
         }
 
 
+        public static bool GetUp(Button input, Controller mask)
+        {
+#if PC
+            return Input.GetButtonUp(input.ToString());
+#elif Oculus
+            return OVRInput.GetUp((OVRInput.Button)input, (OVRInput.Controller)mask);
+#endif
+
+        }
+        
+        // View 재정렬 함수
+        public static void Recenter()
+        {
+            OVRManager.display.RecenterPose();
+        }
+
+
+    }
+
+    class VibrationManager : MonoBehaviour
+    {
+        public static VibrationManager instance;
+
+        private void Awake()
+        {
+            if(instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+
+        /// <summary>
+        /// 퀘스트2 콘트롤러 진동 함수
+        /// </summary>
+        /// <param name="frequency">진동 횟수</param>
+        /// <param name="amplitude">진폭</param>
+        /// <param name="time">시간</param>
+        /// <param name="mask">컨트롤러</param>
+        public void StartViberation(float frequency, float amplitude, float time, MTVS_Input.Controller mask)
+        {
+            StopAllCoroutines();
+
+            StartCoroutine(ControllerVibrationTimer(frequency, amplitude, time, mask));
+        }
+
+
+        /// <summary>
+        /// 진동 코루틴 함수
+        /// </summary>
+        /// <param name="frequency">진동 횟수</param>
+        /// <param name="amplitude">진폭</param>
+        /// <param name="time">시간</param>
+        /// <param name="mask">컨트롤러</param>
+        /// <returns></returns>
+        IEnumerator ControllerVibrationTimer(float frequency, float amplitude, float time, MTVS_Input.Controller mask)
+        {
+            // 진동 시작
+            OVRInput.SetControllerVibration(frequency, amplitude, (OVRInput.Controller)mask);
+
+            yield return new WaitForSeconds(time);
+
+            // 진동 끝
+            OVRInput.SetControllerVibration(0, 0, (OVRInput.Controller)mask);
+        }
     }
 }
 
